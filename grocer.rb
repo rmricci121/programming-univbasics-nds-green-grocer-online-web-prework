f38fd0b35fe2be105486f8662238e2353d89f3bd
Robert M Ricci <rmricci121@gmail.com>
2019-11-19 18:53:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -98,7 +98,7 @@ def checkout(cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0 
-  counter = 2 
+  counter = 0 
   while counter < final_cart.length do 
    total += final_cart[counter][:price] * final_cart[counter][:count]
   counter += 1
