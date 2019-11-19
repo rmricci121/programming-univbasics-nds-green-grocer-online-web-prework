01f10a0a4654531a3b01073653d602d3044055a5
Robert M Ricci <rmricci121@gmail.com>
2019-11-19 18:47:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -95,5 +95,8 @@ def checkout(cart, coupons)
   # some irritated customers
   consolidated cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
-  final_cart = apply_clearance
+  final_cart = apply_clearance(couponed_cart)
+  
+  total = 0 
+  counter = 0
 end
